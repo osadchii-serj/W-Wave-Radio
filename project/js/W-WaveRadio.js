9a4d69c763ed6fbe48a674ae12dc386de404d1a7
Сергей Осадчий <osadchii.serega48@gmail.com>
2023-04-25 16:46:14 +0300

    Добавил разметку header

---
+++ project/js/W-WaveRadio.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", function(){
+//   document.getElementById("log-in").addEventListener("click", function(){
+//     document.querySelector(".header").classList.add("open-form")
+//   });
+//   document.getElementById("close-modal-btn").addEventListener("click", function(){
+//     document.querySelector(".header").classList.remove("open-form")
+//   });
+// });
+
+// document.addEventListener("DOMContentLoaded", function(){
+//   document.getElementById("header-search-btn").addEventListener("click", function(){
+//     document.querySelector(".header").classList.toggle("open-search")
+//   });
+// });
+
+
