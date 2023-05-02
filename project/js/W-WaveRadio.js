9e6a7ad9afe3aa5e52e2ebf2dec0aabe5d696a6c
Сергей Осадчий <osadchii.serega48@gmail.com>
2023-05-02 17:44:24 +0300

    restart

--- project/js/W-WaveRadio.js
+++ project/js/W-WaveRadio.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function(){
-  document.getElementById("header-menu-320").addEventListener("click", function(){
-    document.querySelector(".header__broadcast-menu-320").classList.toggle("open-header-menu-320")
-  });
-});
+// document.addEventListener("DOMContentLoaded", function(){
+//   document.getElementById("header-menu-320").addEventListener("click", function(){
+//     document.querySelector(".header__broadcast-menu-320").classList.toggle("open-header-menu-320")
+//   });
+// });
 
 // document.addEventListener("DOMContentLoaded", function(){
 //   document.getElementById("header-search-btn").addEventListener("click", function(){
