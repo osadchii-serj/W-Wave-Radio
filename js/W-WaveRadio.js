17f91f87ada0130b2e65bbb4f3f9d8312a034e74
Сергей Осадчий <osadchii.serega48@gmail.com>
2023-05-11 15:40:59 +0300

    новая система

---
+++ js/W-WaveRadio.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("burger").addEventListener("click", function(){
+    document.querySelector(".header").classList.toggle("open-menu")
+  });
+
+  document.getElementById("accordion-btn").addEventListener("click", function(){
+    document.querySelector(".header").classList.toggle("open-accordion")
+  });
+
+  document.getElementById("open-btn").addEventListener("click", function(){
+    document.querySelector(".header").classList.add("open-search");
+  });
+
+  document.getElementById("close-btn").addEventListener("click", function(){
+    document.querySelector(".header").classList.remove("open-search");
+  });
+
+  document.getElementById("log-in").addEventListener("click", function(){
+    document.querySelector(".header").classList.add("open-registration-form");
+  });
+
+  document.getElementById("registration-close").addEventListener("click", function(){
+    document.querySelector(".header").classList.remove("open-registration-form");
+  });
+
+});
+
+
+new Accordion('.accordion-container');
+
+let validation =  new JustValidate(".registration-form")
+validation.addField("#name", [
+  {
+    rule: "required",
+    errorMessage: "Введите имя",
+  },
+  {
+    rule: "minLength",
+    value: 2,
+    errorMessage: "Минимум 2 символа",
+  },
+  {
+    rule: 'customRegexp',
+    value: /[a-zA-Zа-яА-Я]+/g,
+    errorMessage: "Только буквы",
+  },
+]);
+validation.addField("#password", [
+  {
+    rule: "required",
+    errorMessage: "Введите пароль",
+  },
+  {
+    rule: "minLength",
+    value: 8,
+    errorMessage: "Минимум 8 символов",
+  },
+]);
+
