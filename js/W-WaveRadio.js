1c2ec8a50a4835cf8978c5088410318a2005df75
Сергей Осадчий <osadchii.serega48@gmail.com>
2023-05-12 21:55:12 +0300

    header правки готов - hero готов

--- js/W-WaveRadio.js
+++ js/W-WaveRadio.js
@@ -1,34 +1,38 @@
-document.addEventListener("DOMContentLoaded", function(){
-  document.getElementById("burger").addEventListener("click", function(){
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("burger").addEventListener("click", function () {
     document.querySelector(".header").classList.toggle("open-menu")
   });
 
-  document.getElementById("accordion-btn").addEventListener("click", function(){
-    document.querySelector(".header").classList.toggle("open-accordion")
-  });
+});
 
-  document.getElementById("open-btn").addEventListener("click", function(){
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("open-btn").addEventListener("click", function () {
     document.querySelector(".header").classList.add("open-search");
   });
 
-  document.getElementById("close-btn").addEventListener("click", function(){
+  document.getElementById("close-btn").addEventListener("click", function () {
     document.querySelector(".header").classList.remove("open-search");
   });
 
-  document.getElementById("log-in").addEventListener("click", function(){
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("log-in").addEventListener("click", function () {
     document.querySelector(".header").classList.add("open-registration-form");
   });
 
-  document.getElementById("registration-close").addEventListener("click", function(){
+  document.getElementById("registration-close").addEventListener("click", function () {
     document.querySelector(".header").classList.remove("open-registration-form");
   });
 
 });
 
 
-new Accordion('.accordion-container');
+const accordion = new Accordion('.accordion-container', {
+  duration: 500,
+});
 
-let validation =  new JustValidate(".registration-form")
+let validation = new JustValidate(".registration-form")
 validation.addField("#name", [
   {
     rule: "required",
