b54d682a55579ff9a18c921030b95789d8c15f9e
Осадчий Сергей <osadchii.serega48@gmail.com>
2023-08-18 17:32:46 +0300

    header__registration-form ready

--- js/w-wave-radio.js
+++ js/w-wave-radio.js
@@ -3,3 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector("header").classList.toggle("header__nav-open")
     })
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("header-login-link").addEventListener("click", function() {
+        document.querySelector("header").classList.add("header__registration-form-open")
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("form-close-btn").addEventListener("click", function() {
+        document.querySelector("header").classList.remove("header__registration-form-open")
+    })
+})
